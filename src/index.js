8e78d49244f5f552631fe1779df2d83f3d96eedf
TrungDT <thanhtrung.it25@gmail.com>
2018-12-23 20:06:59 +0700

    Add redux devtool

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore, applyMiddleware, combineReducers, compose } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+import "./index.css"
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
 
-import * as reducers from './store/reducers';
-const store = createStore(combineReducers(reducers),
-                          applyMiddleware(thunk));
+import * as reducers from "./store/reducers"
+const store = createStore(
+  combineReducers(reducers),
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
-registerServiceWorker();
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
+registerServiceWorker()
--- src/store/topics/reducer.js
+++ src/store/topics/reducer.js
@@ -4,45 +4,44 @@
 // the other exports in this file are selectors, which is business logic that digests parts of the store's state
 // for easier consumption by views
 
-import _ from 'lodash';
-import Immutable from 'seamless-immutable';
-import * as types from './actionTypes';
+import _ from "lodash"
+import Immutable from "seamless-immutable"
+import * as types from "./actionTypes"
 
 const initialState = Immutable({
-    topicsByUrl: undefined,
-    selectedTopicUrls: []
-});
+  topicsByUrl: undefined,
+  selectedTopicUrls: []
+})
 
 export default function reducer(state = initialState, action = {}) {
-    switch (action.type) {
-        case types.TOPICS_FETCHED:
-            return state.merge({
-                topicsByUrl: action.topicsByUrl
-            })
-        case types.TOPICS_SELECTED:
-            return state.merge({
-                selectedTopicUrls: action.selectedTopicUrls
-            })
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case types.TOPICS_FETCHED:
+      return state.merge({
+        topicsByUrl: action.topicsByUrl
+      })
+    case types.TOPICS_SELECTED:
+      return state.merge({
+        selectedTopicUrls: action.selectedTopicUrls
+      })
+    default:
+      return state
+  }
 }
 
 // selectors
 
 export function getTopicsByUrl(state) {
-    return state.topics.topicsByUrl;
+  return state.topics.topicsByUrl
 }
 
 export function getTopicsUrlArray(state) {
-    return _.keys(state.topics.topicsByUrl);
+  return _.keys(state.topics.topicsByUrl)
 }
 
 export function getSelectedTopicUrls(state) {
-    return state.topics.selectedTopicUrls;
-    return _.keyBy(state.topics.selectedTopicUrls);
+  return state.topics.selectedTopicUrls
 }
 
 export function getSelectedTopicUrlsMap(state) {
-    return _.keyBy(state.topics.selectedTopicUrls);
+  return _.keyBy(state.topics.selectedTopicUrls)
 }
