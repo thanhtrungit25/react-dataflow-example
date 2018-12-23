2985978b3409bd19585db8635f04518f4d113ed6
TrungDT <thanhtrung.it25@gmail.com>
2018-12-23 21:52:35 +0700

    fetch posts action

--- src/containers/PostsScreen.js
+++ src/containers/PostsScreen.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
+import * as postsActions from "../store/posts/actions"
 import "./PostsScreen.css"
 
 class PostsScreen extends Component {
+  componentDidMount() {
+    this.props.dispatch(postsActions.fetchPosts())
+  }
   render() {
     return <div className="PostsScreen">PostsScreen</div>
   }
 }
 
-export default PostsScreen
+function mapStateToProps(state) {
+  return {}
+}
+
+export default connect()(PostsScreen)
---
+++ src/store/posts/actions.js
@@ -0,0 +1,21 @@
+import _ from "lodash"
+import * as types from "./actionTypes"
+import redditService from "../../services/reddit"
+import * as topicsSelectors from "../topics/reducer"
+
+export function fetchPosts() {
+  return async (dispatch, getState) => {
+    try {
+      const selectedTopicUrls = topicsSelectors.getSelectedTopicUrls(getState())
+      const fetchPromises = _.map(selectedTopicUrls, topicUrl =>
+        redditService.getPostsFromSubreddits(topicUrl)
+      )
+      const topicPosts = await Promise.all(fetchPromises)
+      console.log("topicPosts", topicPosts)
+      const postsById = _.keyBy(_.flatten(topicPosts), post => post.id)
+      dispatch({ type: types.POSTS_FETCHED, postsById })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
